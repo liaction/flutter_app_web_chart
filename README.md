722f3d1f7068ae5cf6279f6bacd6d8ab6835dcfc
cs <123456>
2019-04-28 11:06:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# flutter_app_web_chart
+
+Flutter & WebView & ECharts.
+
+## lib
+
+- [flutter_inappbrowser](https://pub.flutter-io.cn/packages/flutter_inappbrowser)
+- [eCharts](https://echarts.baidu.com)
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.io/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final title = "Webview & Chart";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        tooltip: 'refresh',
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
